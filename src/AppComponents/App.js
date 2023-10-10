322a88f3fa831371f968100eedd545df582c014a
Michael Mohlomi <Kamogelomohlomi@yahoo.com>
2023-10-10 23:25:00 +0200

    initial playground

---
+++ src/AppComponents/App.js
@@ -0,0 +1,9 @@
+import Homepage from "./Homepage";
+
+function App() {
+  return (
+    <Homepage />
+  );
+}
+
+export default App;
---
+++ src/AppComponents/Homepage.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './Homepage.css';
+import { useQuery } from 'react-query';
+
+function Homepage() {
+
+  useQuery('hello-world', () => {
+     return Promise.resolve(5)   
+  })
+
+  return (
+    <div className="dark">
+    <div className="bg-white dark:bg-[#282c34] text-center">
+      <header className="Home-header">
+        <img src={logo} className="Home-logo" alt="logo"/>
+        <p className="text-gray-300"> 
+          Edit <code>src/AppComponents/Homepage.js</code> and save to reload.
+        </p>
+        <a
+          className="Home-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+    </div>
+  );
+}
+
+export default Homepage;
