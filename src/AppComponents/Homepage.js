011e7fe8d0eb1891b349deb01c0242e954b5782d
Michael Mohlomi <Kamogelomohlomi@yahoo.com>
2023-10-11 12:34:42 +0200

    Addition of Shopping Cart Page

--- src/AppComponents/Homepage.js
+++ src/AppComponents/Homepage.js
@@ -22,7 +22,7 @@ function Homepage() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reac
+          Learn React
         </a>
       </header>
     </div>
